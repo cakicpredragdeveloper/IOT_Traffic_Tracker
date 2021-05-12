0b4db384fdfb52450f929b3a19275d08da2d458c
cakicpredragdeveloper <cakicpredrag@outlook.com>
2021-05-12 22:44:19 +0200

    Kreiranje staticke klase za cuvanje podataka aplikacije

--- IOT_Traffic_Tracker_Microservices/IOT_Traffic_Tracker_Microservices/Controllers/SensorDeviceController.cs
+++ IOT_Traffic_Tracker_Microservices/IOT_Traffic_Tracker_Microservices/Controllers/SensorDeviceController.cs
@@ -4,31 +4,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Sensor_Device_Service.Helpers;
 
-namespace IOT_Traffic_Tracker_Microservices.Controllers
+namespace Sensor_Device_Service.Controllers
 {
     [ApiController]
     [Route("sensor-device-service")]
     public class SensorDeviceController : Controller
     {
-        private readonly ISensorRepository _sensorRepository;
-
-        //kontroler kojim ce da upravlja radom SensorDevice servisa
         public SensorDeviceController(ISensorRepository sensorRepository)
         {
-            //klasa tj interfejs kojim ce da se upravlja podacima
-            //njihovo slanje, primanje
-            //moze i propertiji da se navedu u interferjsu, iskoristicemo ih za kontrolne signale
-            //ne svidja mi se da se zove repository u ovom slucaju ali promenicemo naziv
-            _sensorRepository = sensorRepository;
 
-            //ovde se okida fja iz repository-a za ucitavanje podataka i njihovo slanje drugom servisu
-            //da bi odmah po pokretanju krenuo sa radom
         }
 
-        //TODO -- ruta za postavljanje time limit-a
+        //TODO: POST ruta za postavljanje time limit-a
 
+        //TODO: GET ruta za citanje time limit-a
 
-        //TODO -- ruta za citanje time limit-a
+        //TODO: POST ruta za postavljanje ammount of data
+
+        //TODO: GET ruta za citanje ammount of data
     }
 }
---
+++ IOT_Traffic_Tracker_Microservices/IOT_Traffic_Tracker_Microservices/Helpers/DeviceParameters.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sensor_Device_Service.Helpers
+{
+    public static class DeviceParameters
+    {
+        public static int TimeLimit { get; set; }
+        public static int AmmountOfData { get; set; }
+    }
+}
