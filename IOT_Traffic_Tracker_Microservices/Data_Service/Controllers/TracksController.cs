d085d2ab69bbec13729e29e9d5765e8a5cb699c8
Dimitrije Radosavljevic <dimitrijedimitrije6@gmail.com>
2021-05-18 18:18:59 +0200

    Finish track controller CRUD

--- IOT_Traffic_Tracker_Microservices/Data_Service/Controllers/TracksController.cs
+++ IOT_Traffic_Tracker_Microservices/Data_Service/Controllers/TracksController.cs
@@ -18,11 +18,50 @@ namespace Data_Service.Controllers
             _repo = repo;
         }
 
-        // GET api/todos
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Track>>> Get()
         {
             return new ObjectResult(await _repo.GetAllTracks());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Track>> Get(long id)
+        {
+            var track = await _repo.GetTrack(id);
+            if (track == null)
+                return new NotFoundResult();
+
+            return new ObjectResult(track);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Track>> Post([FromBody] Track track)
+        {
+            track.Id = await _repo.GetNextId();
+            await _repo.Create(track);
+            return new OkObjectResult(track);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Track>> Put(long id, [FromBody] Track track)
+        {
+            var trackFromDb = await _repo.GetTrack(id);
+            if (trackFromDb == null)
+                return new NotFoundResult();
+            track.Id = trackFromDb.Id;
+            track.InternalId = trackFromDb.InternalId;
+            await _repo.Update(track);
+            return new OkObjectResult(track);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            var post = await _repo.GetTrack(id);
+            if (post == null)
+                return new NotFoundResult();
+            await _repo.Delete(id);
+            return new OkResult();
         }
     }
 }
