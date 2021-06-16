9a54e2b947eef404de013d98c3d32d2ea77df385
Dimitrije Radosavljevic <dimitrijedimitrije6@gmail.com>
2021-06-16 08:49:38 +0200

    Add Command Hub

---
+++ IOT_Traffic_Tracker_Microservices/CommandService/HubConfig/CommandHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace CommandService.HubConfig
+{
+    public class CommandHub: Hub
+    {
+        public void sendCommand()
+        {
+            Clients.All.SendAsync("commands", "C O M A N D");
+        }
+    }
+}
